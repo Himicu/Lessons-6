6b7ced90a75779e2d4ff2ee6dcfa654e8c2961c8
hizumi <nyatel@yandex.ru>
2023-05-25 12:54:19 +0400

    add task 42

---
+++ task42/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное. 
+//45 -> 101101
+//3  -> 11
+//2  -> 10
+
+int num;
+Console.Write("Введите номер: ");
+num = int.Parse(Console.ReadLine());
+string rem = "";
+while (num > 0)
+{
+    rem = (num % 2).ToString() + rem;
+    num /= 2;
+}
+Console.WriteLine($"Бинарный формат для заданного номера {rem}");
+
