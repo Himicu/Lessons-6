fb34b374b9e61d3db9bd65b58899496b319fec03
hizumi <nyatel@yandex.ru>
2023-05-25 12:54:02 +0400

    add task 39

---
+++ task39/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+void CraeteArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 10);
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+void ReversArray(int[] arr)
+{
+    int temp = 0;
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[arr.Length - i - 1];
+        arr[arr.Length - i - 1] = temp;
+    }
+}
+int size = new Random().Next(5, 11);
+int[] array = new int[size];
+CraeteArray(array);
+PrintArray(array);
+ReversArray(array);
+PrintArray(array);
